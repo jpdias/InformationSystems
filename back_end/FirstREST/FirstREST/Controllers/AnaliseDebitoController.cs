9a171f77d3d31fe97dc7b79bb8b635cf98976861
Eduardo Martins <edmartins_10@hotmail.com>
2014-12-15 15:17:52 +0000

    Implemented analisedebido and his controller

---
+++ back_end/FirstREST/FirstREST/Controllers/AnaliseDebitoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FirstREST.Lib_Primavera.Model;
+
+using System.Web.Http.Cors;namespace FirstREST.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class AnaliseDebitoController : ApiController
+    {
+        public Lib_Primavera.Model.AnaliseDebito Get()
+        {
+            return Lib_Primavera.Comercial.ListaAnaliseDebito();
+        }
+
+
+        // GET api/artigo/5    
+        public AnaliseDebito Get(string id)
+        {
+            Lib_Primavera.Model.AnaliseDebito analiseDebito = Lib_Primavera.Comercial.GetAnaliseDebito(id);
+            if (analiseDebito == null)
+            {
+                throw new HttpResponseException(
+                  Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            else
+            {
+                return analiseDebito;
+            }
+        }
+
+    }
+}
+
---
+++ back_end/FirstREST/FirstREST/Lib_Primavera/Model/AnaliseDebito/AnaliseDebito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+    public class AnaliseDebito
+    {
+        public List<Model.PendentesAnuais> pendentes
+        {
+            get;
+            set;
+        }
+
+    }
+}
