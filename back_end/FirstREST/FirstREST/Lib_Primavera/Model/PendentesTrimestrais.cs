081a4b0ec73115341e27b77f316cde6bb899880f
Eduardo Martins <edmartins_10@hotmail.com>
2014-12-15 15:16:15 +0000

    Implemented api for pendentesTrimestrais

---
+++ back_end/FirstREST/FirstREST/Lib_Primavera/Model/PendentesTrimestrais.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+    public class PendentesTrimestrais
+    {
+        public int DebitoClientes
+        {
+            get;
+            set;
+        }
+        public int DebitoFornecedores
+        {
+            get;
+            set;
+        }
+        public int DebitoClientesAcumulado
+        {
+            get;
+            set;
+        }
+        public int DebitoFornecedoresAcumulado
+        {
+            get;
+            set;
+        }
+        public List<Model.FaturaPendente> pendentesClientes
+        {
+            get;
+            set;
+        }
+        public List<Model.FaturaPendente> pendentesFornecedores
+        {
+            get;
+            set;
+        }
+
+    }
+}
