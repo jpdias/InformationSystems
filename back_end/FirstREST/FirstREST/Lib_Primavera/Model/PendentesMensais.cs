b041a740a900fa73d695b6e802d912a78fa08920
Eduardo Martins <edmartins_10@hotmail.com>
2014-12-15 15:16:30 +0000

    Implemented ai for pendentesMensais

---
+++ back_end/FirstREST/FirstREST/Lib_Primavera/Model/PendentesMensais.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+    public class PendentesMensais
+    {
+        public int Mes
+        {
+            get;
+            set;
+        }
+        public int DebitoClientes
+        {
+            get;
+            set;
+        }
+        public int DebitoFornecedores
+        {
+            get;
+            set;
+        }
+        public int DebitoClientesAcumulado
+        {
+            get;
+            set;
+        }
+        public int DebitoFornecedoresAcumulado
+        {
+            get;
+            set;
+        }
+        public List<Model.FaturaPendente> pendentesClientes
+        {
+            get;
+            set;
+        }
+        public List<Model.FaturaPendente> pendentesFornecedores
+        {
+            get;
+            set;
+        }
+
+    }
+}
