b178d22254bba21a485f08b0cde36d0f21f2f281
Eduardo Martins <edmartins_10@hotmail.com>
2014-12-17 03:38:06 +0000

    implemented dashboard js controller

---
+++ app/assets/javascripts/controller/dashboard_controller.js
@@ -0,0 +1,16 @@
+function DashboardIndex(){
+
+    loadPage('../dashboard/index.html');
+
+    /*var _url = 'http://localhost:49822/api/AnaliseDebito';
+
+    $.ajax({
+        url: _url,
+        type: 'GET',
+        contentType: "application/json;charset=UTF-8"
+
+    }).done(function(data){  
+
+        listarPendentes('anual',data);
+    });*/ 
+}
