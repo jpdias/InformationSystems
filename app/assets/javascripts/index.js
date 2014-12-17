dcba57c39cf09695b9fcb83982fabb3bd1a6dcf6
Eduardo Martins <edmartins_10@hotmail.com>
2014-12-17 03:30:45 +0000

    Added function to convert mes_id to mes_string

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,3 +2,43 @@ function loadPage(_url){
     $('#page-wrapper').empty();
     $('#page-wrapper').load(_url);
 }
+
+function convertMesIdToString(mes_id){
+    if(mes_id == 1)
+        return 'Janeiro';
+    else
+        if(mes_id == 2)
+            return 'Fevereiro';
+        else
+            if(mes_id == 3)
+                return 'Março';
+            else
+                if(mes_id == 4)
+                    return 'Abril';
+                else
+                    if(mes_id == 5)
+                        return 'Maio';
+                    else
+                        if(mes_id == 6)
+                            return 'Junho';
+                        else
+                            if(mes_id == 7)
+                                return 'Julho';
+                            else
+                                if(mes_id == 8)
+                                    return 'Agosto';
+                                else
+                                    if(mes_id == 9)
+                                        return 'Setembro';
+                                    else
+                                        if(mes_id == 10)
+                                            return 'Outubro';
+                                        else
+                                            if(mes_id == 11)
+                                                return 'Novembro';
+                                            else
+                                                if(mes_id == 12)
+                                                    return 'Dezembro';
+                                                else
+                                                    return 'Unknown';
+}
